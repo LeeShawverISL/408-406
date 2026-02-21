e8812b689ee3b47db90f8e725dd910770d20f10d
Lee Shawver <165132276+LeeShawverISL@users.noreply.github.com>
2026-02-21 15:08:13 +0000

    added background change

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const storyBox = document.getElementById('fr-story-display');
 
 // 2. Add the "Click" instruction
 startBtn.addEventListener('click', function() {
-    // Start the music
+    document.body.style.backgroundImage = "url('whiteboard_FR.jpg')";
     music.play();
     
     // Optional: Lower the volume so it's not too loud (0.0 to 1.0)
@@ -13,5 +13,6 @@ startBtn.addEventListener('click', function() {
 
     // Transition to the next part of your game
     storyBox.style.display = 'none'; 
+    document.getElementById('fr-puzzle-area').classList.remove('hidden');
     console.log("Music started and story hidden!");
 });
